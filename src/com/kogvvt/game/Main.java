a6fb3a007e50565bbec070ab26f560028800b177
kogvvt <kogvvt@icloud.com>
2020-09-08 12:05:14 +0200

    Added main game handler to main.java

--- src/com/kogvvt/game/Main.java
+++ src/com/kogvvt/game/Main.java
@@ -1,10 +1,21 @@
 package com.kogvvt.game;
 
+import javax.swing.JFrame;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Game game = new Game();
+		
+		JFrame window = new JFrame("2048");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(false);
+		window.add(game);
+		window.pack();
+		window.setLocationRelativeTo(null);
+		window.setVisible(true);
+		
+		game.start();
 	}
 
 }
