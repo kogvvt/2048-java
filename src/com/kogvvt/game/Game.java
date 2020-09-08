cec17827e5e008b712133e97106e54d9626da107
kogvvt <kogvvt@icloud.com>
2020-09-08 12:46:18 +0200

    Updated the Game class

--- src/com/kogvvt/game/Game.java
+++ src/com/kogvvt/game/Game.java
@@ -12,13 +12,16 @@ import java.awt.image.renderable.RenderableImage;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
+import jdk.internal.jshell.tool.StopDetectingInputStream;
+import jdk.javadoc.internal.tool.Start;
+
 public class Game extends JPanel implements KeyListener, Runnable {
 
 	
 	private static final long serialVersionUID = 1L;
 	public static final int width = 400;
 	public static final int height = 630;
-	public static final Font Main = new Font("Courier",Font.PLAIN, 28);
+	public static final Font main = new Font("Courier",Font.PLAIN, 28);
 	private Thread game;
 	private boolean running;
 	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
@@ -58,43 +61,62 @@ public class Game extends JPanel implements KeyListener, Runnable {
 		int fps = 0;
 		int updates = 0;
 		long fpsTimer = System.currentTimeMillis();
-		double nsPerUpdate = 1000000000.0 /60;
-		boolean shouldRender  = false;
+		double nsPerUpdate = 1000000000.0 / 60;
 		
 		// last update time in ns
 		double then = System.nanoTime();
 		double unprocessed = 0;
 		
-		//update queue
-		while(unprocessed >=1) {
-			updates++;
-			update();
-			unprocessed--;
-			shouldRender = true;
+		while(running) {
+			boolean shouldRender  = false;
+			double now = System.nanoTime();
+			unprocessed += (now - then) / nsPerUpdate;
+			then = now;
 			
-		}
-		//render
-		
-		if(shouldRender){
-			fps++;
-			render();
-			shouldRender = false;
-		}else{
-			try{
-				Thread.sleep(1);
-			}catch (Exception e){
-				e.printStackTrace();
+		//update queue
+			while(unprocessed >=1) {
+				updates++;
+				update();
+				unprocessed--;
+				shouldRender = true;
+				
+			}
+			//render
+			
+			if(shouldRender){
+				fps++;
+				render();
+				shouldRender = false;
+			}else{
+				try{
+					Thread.sleep(1);
+				}catch (Exception e){
+					e.printStackTrace();
+				}
+			}
+			
+			// FPS Timer
+			if(System.currentTimeMillis()-fpsTimer >1000) {
+				System.out.printf("%d fps %d udpates",fps,updates);
+				System.out.println();
+				fps = 0;
+				updates = 0;
+				fpsTimer += 1000;
 			}
 		}
-		
-		// FPS Timer
-		if(System.currentTimeMillis()-fpsTimer >1000) {
-			System.out.printf("%d fps %d udpates",fps,updates);
-			System.out.println();
-			fps = 0;
-			updates = 0;
-			fpsTimer += 1000;
-		}
+	}
+	
+	public synchronized void start() {
+		if(running) return;
+		running = true;
+		game = new Thread(this, "game");
+		game.start();
+	}
+	
+	public synchronized void stop() {
+		if(!running) return;
+		running = false;
+		System.exit(0);
 	}
 
 	@Override
